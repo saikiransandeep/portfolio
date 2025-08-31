7c1298bcc85a1209bedf2066880b77b95031e957
Sai Kiran <saikiransandeep@gmail.com>
2025-08-31 19:34:43 +0530

    feat: Implement dark mode styling and theme toggle functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,13 @@ tailwind.config = {
       colors: {
         lightHover: "#fcf4ff",
         darkHover: "#2a004a",
+        darkTheme: "#11002F",
       },
       boxShadow: {
-        black: "4px_4px_0_#000",
-        white: "4px_4px_0_#fff",
+        black: "4px 4px 0 #000",
+        white: "4px 4px 0 #fff",
       },
     },
   },
+  darkMode: "selector",
 };
