0b39af724509ab17301ebfe7b7fd9c6a2fc8e071
Sai Kiran <saikiransandeep@gmail.com>
2025-08-31 16:36:33 +0530

    feat: Update grid layout and hover effects in portfolio sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 tailwind.config = {
   theme: {
-    gridTemplateColumns: {
-      auto: "repeat(auto-fit, minmax(200px, 1fr))",
-    },
-    fontFamily: {
-      Outfit: ["Outfit", "sans-serif"],
-      Ovo: ["Ovo", "serif"],
-    },
-    animation: {
-      spin_slow: "spin 6s linear infinite",
+    extend: {
+      gridTemplateColumns: {
+        auto: "repeat(auto-fit, minmax(200px, 1fr))",
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+      animation: {
+        spin_slow: "spin 6s linear infinite",
+      },
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+      },
+      boxShadow: {
+        black: "4px_4px_0_#000",
+        white: "4px_4px_0_#fff",
+      },
     },
   },
 };
